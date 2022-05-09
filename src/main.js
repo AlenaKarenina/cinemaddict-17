da835dea7127d9b7681669f1e4c66d37f6260887
Alena Karenina <karenina@corp.profi.travel>
2022-05-10 00:34:26 +0500

    added new view + showing cards by groups

--- src/main.js
+++ src/main.js
@@ -1,23 +1,19 @@
-import {render, RenderPosition} from './render.js';
+import {render} from './render.js';
 import ProfileView from './view/profile-view.js';
 import NavigationView from './view/main-navigation-view.js';
-import FilterView from './view/filter-view.js';
 import FooterStatisticsView from './view/footer-statistics-view.js';
-
 import FilmsPresenter from './presenter/main-films-presenter.js';
-
 import MovieModel from './model/movie-model.js';
 
 const pageHeader = document.querySelector('.header');
 const pageMain = document.querySelector('.main');
 const pageFooter = document.querySelector('.footer');
 
-const containerFilmsPresenter = new FilmsPresenter;
 const movieModel = new MovieModel();
+const containerFilmsPresenter = new FilmsPresenter(pageMain, movieModel);
 
 render(new ProfileView, pageHeader);
-render(new NavigationView, pageMain, RenderPosition.BEFOREEND);
-render(new FilterView, pageMain, RenderPosition.BEFOREEND);
-render(new FooterStatisticsView, pageFooter, RenderPosition.BEFOREEND);
+render(new NavigationView, pageMain);
+render(new FooterStatisticsView, pageFooter);
 
-containerFilmsPresenter.init(pageMain, movieModel);
+containerFilmsPresenter.init();
