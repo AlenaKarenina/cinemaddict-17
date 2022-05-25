07a334555e1ec4b3449ea4f5e3ed0c738f46e31e
Alena Karenina <karenina@corp.profi.travel>
2022-05-25 18:42:11 +0500

    Добавит дату комментария

--- src/utils/task.js
+++ src/utils/task.js
@@ -2,8 +2,10 @@ import dayjs from 'dayjs';
 
 const humanizeDueDate = (dueDate) => dayjs(dueDate).format('YYYY');
 
+const humanizeCommentDateTime = (date) => dayjs(date).format('YYYY/MM/DD HH:mm');
+
 const sortFilmsByRating = (filmA, filmB) => (filmB.filmInfo.totalRating - filmA.filmInfo.totalRating);
 
 const sortFilmsByDate = (filmA, filmB) => (dayjs(filmB.filmInfo.release.date).diff(dayjs(filmA.filmInfo.release.date)));
 
-export {humanizeDueDate, sortFilmsByRating, sortFilmsByDate};
+export {humanizeDueDate, sortFilmsByRating, sortFilmsByDate, humanizeCommentDateTime};
