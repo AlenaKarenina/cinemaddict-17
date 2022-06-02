bf2cf550b7e5066aa32467bf2e75c0f96f198876
Alena Karenina <karenina@corp.profi.travel>
2022-06-02 16:08:21 +0500

    Добавит презентер фильтров

--- src/main.js
+++ src/main.js
@@ -1,18 +1,10 @@
 import {render} from './framework/render.js';
 import ProfileView from './view/profile-view.js';
-import FilterView from './view/filter-view.js';
 import FooterStatisticsView from './view/footer-statistics-view.js';
 import FilmsPresenter from './presenter/films-presenter.js';
+import FilterPresenter from './presenter/filter-presenter.js';
 import MovieModel from './model/movie-model.js';
 import FilterModel from './model/filter-model.js';
-
-const filters = [
-  {
-    type: 'all',
-    name: 'ALL',
-    count: 0,
-  },
-];
 
 const pageHeader = document.querySelector('.header');
 const pageMain = document.querySelector('.main');
@@ -21,10 +13,11 @@ const pageFooter = document.querySelector('.footer');
 const movieModel = new MovieModel();
 const filterModel = new FilterModel();
 const containerFilmsPresenter = new FilmsPresenter(pageMain, movieModel);
+const filterPresenter = new FilterPresenter(pageMain, filterModel, movieModel);
 
 render(new ProfileView, pageHeader);
-render(new FilterView(filters, 'all'), pageMain);
 
 render(new FooterStatisticsView(movieModel.count), pageFooter);
 
+filterPresenter.init();
 containerFilmsPresenter.init();
