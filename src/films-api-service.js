7e53613af1130c403f587dfe2de040a5c2130ff9
Alena Karenina <karenina@corp.profi.travel>
2022-06-05 22:00:18 +0500

    Реализует паттерн Адаптер

--- src/films-api-service.js
+++ src/films-api-service.js
@@ -15,12 +15,52 @@ export default class FilmsApiService extends ApiService {
     const response = await this._load({
       url: `movies/${movie.id}`,
       method: Method.PUT,
-      body: JSON.stringify(movie),
+      body: JSON.stringify(this.#adaptToServer(movie)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
+  };
+
+  #adaptToServer = (movie) => {
+    const filmInfo = movie.filmInfo;
+    const userDetails = movie.userDetails;
+
+    const adaptedFilmInfo = {...filmInfo,
+      'alternative_title': filmInfo.alternativeTitle,
+      'total_rating': filmInfo.totalRating,
+      'age_rating': filmInfo.ageRating,
+
+      release: {...filmInfo.release,
+        'release_country': filmInfo.release.releaseCountry
+      }
+    };
+
+    delete adaptedFilmInfo.alternativeTitle;
+    delete adaptedFilmInfo.totalRating;
+    delete adaptedFilmInfo.ageRating;
+    delete adaptedFilmInfo.release.releaseCountry;
+
+    const adaptedUserDetails = {...userDetails,
+      'already_watched': userDetails.alreadyWatched,
+      'watching_date': userDetails.watchingDate,
+    };
+
+    delete adaptedUserDetails.alreadyWatched;
+    delete adaptedUserDetails.watchingDate;
+
+    const adaptedMovie = {...movie,
+      comments: movie.commentsIds,
+      'film_info': adaptedFilmInfo,
+      'user_details': adaptedUserDetails,
+    };
+
+    delete adaptedMovie.commentsIds;
+    delete adaptedMovie.filmInfo;
+    delete adaptedMovie.userDetails;
+
+    return adaptedMovie;
   };
 }
--- src/model/movie-model.js
+++ src/model/movie-model.js
@@ -12,7 +12,7 @@ export default class MovieModel extends Observable {
     this.#filmsApiService = filmsApiService;
 
     this.#filmsApiService.movies.then((movies) => {
-      console.log(movies);
+      console.log(movies.map(this.#adaptToClient));
       // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
       // а ещё на сервере используется snake_case, а у нас camelCase.
       // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
@@ -42,5 +42,46 @@ export default class MovieModel extends Observable {
     ];
 
     this._notify(updateType, update);
+  };
+
+  #adaptToClient = (movie) => {
+    const filmInfo = movie.film_info;
+    const userDetails = movie.user_details;
+
+    const adaptedFilmInfo = {...filmInfo,
+      alternativeTitle: filmInfo.alternative_title,
+      totalRating: filmInfo.total_rating,
+      ageRating: filmInfo.age_rating,
+
+      release: {
+        ...filmInfo.release,
+        releaseCountry: filmInfo.release.release_country
+      }
+    };
+
+    delete adaptedFilmInfo.alternative_title;
+    delete adaptedFilmInfo.total_rating;
+    delete adaptedFilmInfo.age_rating;
+    delete adaptedFilmInfo.release.release_country;
+
+    const adaptedUserDetails = {...userDetails,
+      alreadyWatched: userDetails.already_watched,
+      watchingDate: userDetails.watching_date,
+    };
+
+    delete adaptedUserDetails.already_watched;
+    delete adaptedUserDetails.watching_date;
+
+    const adaptedMovie = {...movie,
+      commentsIds: movie.comments,
+      filmInfo: adaptedFilmInfo,
+      userDetails: adaptedUserDetails,
+    };
+
+    delete adaptedMovie.comments;
+    delete adaptedMovie.film_info;
+    delete adaptedMovie.user_details;
+
+    return adaptedMovie;
   };
 }
